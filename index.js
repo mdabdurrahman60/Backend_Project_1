bfe1aa2dc04e8b4f1000a6282d19450e82c2bb2c
Md.Abdur Rahman <ar.minhazul@gmail.com>
2024-10-14 09:30:42 +0600

    edit option added

--- index.js
+++ index.js
@@ -1,51 +1,44 @@
 const express = require('express');
 const app = express();
-const path = require('path')
-const fs = require('fs')
+const path = require('path');
+const fs = require('fs');
 
-app.set("view engine","ejs");
-
-app.use(express.json());
+//setting up parsers  for form
+app.use(express.json())
 app.use(express.urlencoded({extended:true}));
+//path -> we are using static file for the code down below
+app.use(express.static(path.join(__dirname,'public')))
 
-
-app.use(express.static(path.join(__dirname,"public")));
-
-
+//setting up ejs + we can render for this code 
+app.set('view engine','ejs');
 app.get('/',(req,res)=>{
-    fs.readdir(`./files`,(err,files)=>{
-        res.render("index",{files:files})
-        
+    fs.readdir(`./files`, function(err,files){
+        res.render('index',{files: files});
     })
+    
 });
 app.get('/file/:fileName',(req,res)=>{
     fs.readFile(`./files/${req.params.fileName}`,"utf-8", function(err,fileData){
         res.render('show',{fileName: req.params.fileName,fileData: fileData})
     })    
 });
+app.get('/edit/:fileName',(req,res)=>{
+    res.render('edit',{fileName:req.params.fileName});
+});
 
-
-
+app.post('/edit',(req,res)=>{
+    fs.rename(`./files/${req.body.prevName}`,`./files/${req.body.newName}`,(err)=>{
+        res.redirect("/")
+    })
+});
 
 
 
 app.post('/create',(req,res)=>{
-    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details,(err)=>{
+    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`,req.body.details,(err)=>{
         res.redirect("/")
     })
-})
     
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.listen(3000)
+});
+app.listen(3000);
+ 
